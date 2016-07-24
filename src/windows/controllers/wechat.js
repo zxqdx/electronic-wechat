da38ef941e050c421d9335abecd7477b94e62317
zxqdx <zxq001zxq001@gmail.com>
2016-07-23 21:14:26 -0700

    Customized user item background

--- src/windows/controllers/wechat.js
+++ src/windows/controllers/wechat.js
@@ -1,11 +1,19 @@
 /**
  * Created by Zhongyi on 5/2/16.
+ *
+ * Modified by zxqdx on July 2016.
  */
 "use strict";
 
 const path = require('path');
 const {app, shell, BrowserWindow} = require('electron');
 const Common = require('../../common');
+let Secret;
+try {
+  Secret = require("../../secret");
+} catch (e) {
+  Secret = false;
+}
 
 const CSSInjector = require('../../inject/css');
 const MessageHandler = require('../../handlers/message');
@@ -83,6 +91,33 @@ class WeChatWindow {
       }
 
       new UpdateHandler().checkForUpdate(`v${app.getVersion()}`, true);
+    });
+
+    this.wechatWindow.webContents.on('did-finish-load', () => {
+      if (Secret) {
+        console.log("Len=" + Secret.highlightedUsers[0].animatedImg.length);
+        let script = Secret.highlightedUsers.map(user => `
+          var tempDiv = $('.chat_item .nickname span:contains("${user.username}")');
+          if (tempDiv.length > 0) {
+            var tempUsername =
+              JSON.parse(tempDiv.first().parent().parent().parent().attr("data-cm")).username;
+            var tempCSS = document.createElement("style");
+            tempCSS.type = "text/css";
+            tempCSS.innerHTML = 'div.chat_item[data-cm*="' + tempUsername + '"] {' +
+              'background-image: url("${user.staticImg}");' +
+              'background-repeat: no-repeat;' +
+            '}' +
+            'div.chat_item[data-cm*="' + tempUsername + '"]:hover,' +
+            'div.chat_item.active[data-cm*="' + tempUsername + '"] {' +
+              'background-image: url("${user.animatedImg}");' +
+              'background-repeat: no-repeat;'
+            '}';
+            document.body.appendChild(tempCSS);
+          }
+        `).join();
+        this.wechatWindow.webContents.executeJavaScript(script);
+      }
+      
     });
 
     this.wechatWindow.webContents.on('new-window', (event, url) => {
